0aa52e51b4fe4de4bb7bf791bfeaf3f9df226bc2
Sophie Cooper <sophie@cooperzone.net>
2016-04-28 11:48:43 -0700

    home segue working

---
+++ WineMeUp/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  WineMeUp
+//
+//  Created by Sophie Cooper on 4/28/16.
+//  Copyright © 2016 loretta chan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBAction func unwindToHome(segue: UIStoryboardSegue) {}
+    
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        let nextView = segue.destinationViewController as! HomeViewController;
+//    }
+}
--- WineMeUp/PriceRangeViewController.swift
+++ WineMeUp/PriceRangeViewController.swift
@@ -10,44 +10,30 @@ import Foundation
 import UIKit
 
 class PriceRangeViewController: UIViewController {
-    
-    @IBOutlet var zeroToTenButton: UIButton!
-    @IBOutlet var tenToTwentyBUtton: UIButton!
-    @IBOutlet var TwentyToThirtyButton: UIButton!
-    @IBOutlet var ThirtyUpButton: UIButton!
+
+    @IBOutlet var underTwentyFiveButton: UIButton!
+    @IBOutlet var overTwentyFiveButton: UIButton!
     let prefs = NSUserDefaults.standardUserDefaults();
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        zeroToTenButton.layer.borderWidth = 5;
-        zeroToTenButton.layer.cornerRadius = 7;
-        zeroToTenButton.layer.borderColor = UIColor.blackColor().CGColor
-        tenToTwentyBUtton.layer.borderWidth = 5;
-        tenToTwentyBUtton.layer.cornerRadius = 7;
-        tenToTwentyBUtton.layer.borderColor = UIColor.blackColor().CGColor
-        TwentyToThirtyButton.layer.borderWidth = 5;
-        TwentyToThirtyButton.layer.cornerRadius = 7;
-        TwentyToThirtyButton.layer.borderColor = UIColor.blackColor().CGColor
-        ThirtyUpButton.layer.borderWidth = 5;
-        ThirtyUpButton.layer.cornerRadius = 7;
-        ThirtyUpButton.layer.borderColor = UIColor.blackColor().CGColor
+        underTwentyFiveButton.layer.borderWidth = 5;
+        underTwentyFiveButton.layer.cornerRadius = 7;
+        underTwentyFiveButton.layer.borderColor = UIColor.blackColor().CGColor
+        overTwentyFiveButton.layer.borderWidth = 5;
+        overTwentyFiveButton.layer.cornerRadius = 7;
+        overTwentyFiveButton.layer.borderColor = UIColor.blackColor().CGColor
         
         
     }
     
-    @IBAction func zeroToTenButtonClicked(sender: AnyObject) {
-        prefs.setValue(1, forKey: "priceRange")
+    @IBAction func underTwentyFiveButtonClicked(sender: AnyObject) {
+        //prefs.setValue(1, forKey: "priceRange")
+        print("cheaper")
     }
     
-    @IBAction func tenToTwentyButtonClicked(sender: AnyObject) {
-        prefs.setValue(2, forKey: "priceRange")
-    }
-    
-    @IBAction func twentyToThirtyButtonClicked(sender: AnyObject) {
-        prefs.setValue(3, forKey: "priceRange")
-    }
-    
-    @IBAction func thirtyUpButtonClicked(sender: AnyObject) {
-        prefs.setValue(4, forKey: "priceRange")
+    @IBAction func overTwentyFiveButtonClicked(sender: AnyObject) {
+        //prefs.setValue(2, forKey: "priceRange")
+        print("make it rain")
     }
 }
